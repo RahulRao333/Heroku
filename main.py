5fe6a4dc0281b701eef429f2f6bd3211ed9061b9
RAHUL V RAO <83028163+RahulRao333@users.noreply.github.com>
2022-01-29 02:34:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pickle
+
+from flask import Flask,render_template,request,jsonify
+import numpy as np
+app=Flask(__name__)
+model=pickle.load(open('models.pkl','rb'))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final_features=[np.array(int_features)]
+    prediction=model.predict(final_features)
+
+
+
+    output = prediction
+
+    return render_template('index.html',prediction_text='The result of stroke prediction system is {}'.format(output))
+if __name__ == '__main__':
+    app.run(debug=True)
